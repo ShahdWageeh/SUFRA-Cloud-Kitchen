c9298fe5e962c4a637e8fee1575a955fee2d9e61
Mustafa Osama <1900565@eng.asu.edu.eg>
2026-06-10 09:26:07 +0300

    editing orders

--- src/components/chef/orders/OrdersList.jsx
+++ src/components/chef/orders/OrdersList.jsx
@@ -3,10 +3,11 @@ import OrderCard from "./OrderCard";
 export default function OrdersList({ orders }) {
   return (
     <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
-      {orders.map((order) => (
+      {orders.map((order, index) => (
         <OrderCard
-          key={order._id || order.id || order.orderId}
+          key={order._id}
           order={order}
+          orderNumber={index + 1}
         />
       ))}
     </div>
--- src/components/chef/orders/OrdersTabs.jsx
+++ src/components/chef/orders/OrdersTabs.jsx
@@ -1,18 +1,18 @@
-export default function OrdersTabs() {
+export default function OrdersTabs({incomingOrders}) {
   return (
     <div className="border-b border-surface-low">
       <div className="flex gap-10">
 
         <button className="pb-3 border-b-2 border-primary text-primary font-medium">
-          Incoming (3)
+          Incoming ({incomingOrders})
         </button>
 
         <button className="pb-3 text-text-secondary">
-          In Progress (5)
+          In Progress (0)
         </button>
 
         <button className="pb-3 text-text-secondary">
-          Completed
+          Completed (0)
         </button>
 
       </div>
